ba5a6ee9dc4d6c24436d775d16ba6433262f932b
Adoramis <luzihan1104@163.com>
2026-02-14 13:06:49 +0800

    initial commit

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterConstants {
+    public static String canBus = "rio";
+
+    public static int leftShooterMotorCanId = 20;
+    public static int rightShooterMotorCanId = 21;
+}
