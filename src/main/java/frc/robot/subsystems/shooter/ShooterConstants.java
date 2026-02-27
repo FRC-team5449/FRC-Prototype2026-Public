21e79400296bab4c7416df0c7dc98e4f66c0f0b5
Adoramis <luzihan1104@163.com>
2026-02-27 19:23:08 +0800

    new drive

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.shooter;
-
-public class ShooterConstants {
-    public static String canBus = "rio";
-
-    public static int leftShooterMotorCanId = 20;
-    public static int rightShooterMotorCanId = 21;
-}
