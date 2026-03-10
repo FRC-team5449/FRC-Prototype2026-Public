0643fce9d205264aaf9ef3bf556c02e29e121d83
AmuroRX-93 <leonyuzhewang@icloud.com>
2026-03-11 01:27:06 +0800

    flip fix

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -14,6 +14,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.util.FlippingUtil;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -197,6 +198,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private void configureAutoBuilder() {
         try {
             var config = RobotConfig.fromGUISettings();
+            FlippingUtil.symmetryType = FlippingUtil.FieldSymmetry.kRotational;
 
             AutoBuilder.configure(
                 () -> getState().Pose,
